f9bb9ccb33616b6b65795eb5912710d3c0d38043
Matthew Clapp <itsayellow+dev@gmail.com>
2020-11-29 17:59:25 -0800

    Switch to PEP517 project.

--- setup.py
+++ setup.py
@@ -1,38 +1,7 @@
-# setup for datadiary
+#!/usr/bin/env python3
 
-# import os.path
-from setuptools import setup, find_packages
+import sys
 
-# here = os.path.abspath(os.path.dirname(__file__))
+from setuptools import setup  # type: ignore
 
-setup(
-    name="movies2ical",
-    version="0.3.0",
-    description="Generate ical files from Stanford Movie Theater calendar webpage.",
-    author="Matthew A. Clapp",
-    author_email="itsayellow+dev@gmail.com",
-    license="MIT",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-    ],
-    #keywords="report",
-    url="https://github.com/itsayellow/stanfordmovies",
-    # include_package_data=True,  # so we get html files in datadiary/templates
-    packages=["movies2ical"],
-    install_requires=[
-        "bleach",
-        "beautifulsoup4",
-        "icalendar",
-        "html5lib",
-        "imdbpy>=6.5",
-        "pytz",
-        "requests",
-        "toml",
-        "tzlocal",
-    ],
-    entry_points={"console_scripts": ["movies2ical=movies2ical.main:cli"]},
-    python_requires=">=3.3",
-)
+setup()
